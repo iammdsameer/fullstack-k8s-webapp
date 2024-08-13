006e41766c884939a25b9c81e2e18a2d1c931ae6
MD Sameer <hi@mdsameer.com>
2024-08-13 15:51:21 +0000

    feat: add list and post api

--- backend/app.ts
+++ backend/app.ts
@@ -1,11 +1,38 @@
-import express from "express";
 import "dotenv/config";
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", function (req, res) {
-  res.send("Hello World");
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test")
+  .then(() => console.log("Connected to MongoDB!"));
+
+const schema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+  },
+});
+
+const Newsletter = mongoose.model("Newsletter", schema);
+
+app.post("/api/new", function (req, res) {
+  const { email } = req.body;
+  console.log({ email });
+  const doc = new Newsletter({ email });
+  doc
+    .save()
+    .then(() => res.send("You are officially added to the newsletter!"))
+    .catch((err) => res.send("Error"));
+});
+
+app.get("/api/list", async function (req, res) {
+  const result = await Newsletter.find().select("email id");
+  res.send(result);
 });
 
 app.listen(PORT, () => {
